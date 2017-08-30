841d36ae89b1043d32ec06dbc6eb2aada63deaaa
Timothy Robinson <timmyrobby@gmail.com>
2017-08-30 16:53:47 +0100

    Ensure email body is processed correctly

--- src/lib/email-builder.js
+++ src/lib/email-builder.js
@@ -4,7 +4,7 @@ const omit = require('lodash/omit')
 const emailBuilder = function (email) {
   const builder = new MimeBuilder('text/html')
   return builder
-    .setHeader(omit(email, [email.body]))
+    .setHeader(omit(email, 'body'))
     .setContent(email.body)
     .build()
 }
