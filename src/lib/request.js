23ec1770728bc1dd789e914aeed73dc74f2f4bb6
Nick Collings <nick@collingo.com>
2017-11-14 14:04:12 +0000

    Add async/await to request

--- src/lib/request.js
+++ src/lib/request.js
@@ -15,23 +15,24 @@ if (process.title.includes('node')) {
   config.baseURL = 'http://localhost:82/'
 }
 const axios = Axios.create(config)
-module.exports = (...args) => {
+module.exports = async (...args) => {
   const requestId = shortid.generate()
-  return axios(...args)
-    .then(response => response.data)
-    .catch(error => {
-      switch (get(error, 'response.status')) {
-        case 401:
-          throw new Unauthorized(
-            { type: error.response.data },
-            'request',
-            requestId,
-            ...args
-          )
-        case 404:
-          throw new NotFound('request', requestId, ...args)
-        default:
-          throw new AppError('request', requestId, ...args, error.message)
-      }
-    })
+  try {
+    const response = await axios(...args)
+    return response.data
+  } catch (error) {
+    switch (get(error, 'response.status')) {
+      case 401:
+        throw new Unauthorized(
+          { type: error.response.data },
+          'request',
+          requestId,
+          ...args
+        )
+      case 404:
+        throw new NotFound('request', requestId, ...args)
+      default:
+        throw new AppError('request', requestId, ...args, error.message)
+    }
+  }
 }
