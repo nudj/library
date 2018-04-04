4bd50fb93cdc5b01396149f679c05ca43a884847
Timothy Robinson <timmyrobby@gmail.com>
2018-04-04 20:24:28 +0100

    Update id generation to be exported as a function

--- src/lib/hash/generate-id.js
+++ src/lib/hash/generate-id.js
@@ -1,31 +1,39 @@
 // NB: In use in production - DO NOT EDIT
 
-const generateHash = require('./generate-hash')
+const nodeCrypto = require('crypto')
 const { idTypes } = require('../constants')
 
-const { COMPANY, ROLE, PERSON, CONNECTION } = idTypes
+module.exports = (cryptoAlgorithm) => {
+  const crypto = cryptoAlgorithm || nodeCrypto
+  const { COMPANY, ROLE, PERSON, CONNECTION } = idTypes
 
-const generateIdByKey = (type, key) => (object) => {
-  if (!object[key]) throw new Error(`Invalid ${type}`)
-  return generateHash(object[key])
+  const generateHash = (input) => crypto
+    .createHash('md5')
+    .update(input || crypto.randomBytes(20).toString('hex'))
+    .digest('hex')
+
+  const generateIdByKey = (type, key) => (object) => {
+    if (!object[key]) throw new Error(`Invalid ${type}`)
+    return generateHash(object[key])
+  }
+
+  const generateIdForConnection = (object) => {
+    if (!object.person || !object.from) throw new Error('Invalid connection')
+    return generateHash(`${object.person}.${object.from}`)
+  }
+
+  const idGenerators = {
+    [COMPANY]: generateIdByKey(COMPANY, 'name'),
+    [ROLE]: generateIdByKey(ROLE, 'name'),
+    [PERSON]: generateIdByKey(PERSON, 'email'),
+    [CONNECTION]: generateIdForConnection
+  }
+
+  const generateId = (type, object) => {
+    if (!type || !object || !idGenerators[type]) return generateHash()
+
+    return idGenerators[type](object)
+  }
+
+  return generateId
 }
-
-const generateIdForConnection = (object) => {
-  if (!object.person || !object.from) throw new Error('Invalid connection')
-  return generateHash(`${object.person}.${object.from}`)
-}
-
-const idGenerators = {
-  [COMPANY]: generateIdByKey(COMPANY, 'name'),
-  [ROLE]: generateIdByKey(ROLE, 'name'),
-  [PERSON]: generateIdByKey(PERSON, 'email'),
-  [CONNECTION]: generateIdForConnection
-}
-
-const generateId = (type, object) => {
-  if (!type || !object || !idGenerators[type]) return generateHash()
-
-  return idGenerators[type](object)
-}
-
-module.exports = generateId
