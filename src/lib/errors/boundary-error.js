ad58498e0cf92e6a1df29940889b626a13eb998f
Timothy Robinson <timmyrobby@gmail.com>
2018-07-18 14:39:47 +0100

    minor: Update `BoundaryError` to use constant

--- src/lib/errors/boundary-error.js
+++ src/lib/errors/boundary-error.js
@@ -1,9 +1,11 @@
+const { GENERIC } = require('./constants')
+
 class BoundaryError extends Error {
   constructor (name, options = {}) {
     options = typeof options === 'string' ? { message: options } : options
     super(options.message || 'Something went wrong')
     this.name = name
-    this.code = options.code || 'GENERIC'
+    this.code = options.code || GENERIC
     this.log = options.log || []
   }
 }
