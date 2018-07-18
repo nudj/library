a1eb819650498b1d81c750b629a276a527062f27
Timothy Robinson <timmyrobby@gmail.com>
2018-07-18 14:37:33 +0100

    minor: Add error constants

---
+++ src/lib/errors/constants.js
@@ -0,0 +1,14 @@
+const GENERIC = 'GENERIC'
+const ALREADY_EXISTS = 'ALREADY_EXISTS'
+const INVALID = 'INVALID'
+
+const statusCodes = {
+  BAD_REQUEST: 400
+}
+
+module.exports = {
+  GENERIC,
+  INVALID,
+  ALREADY_EXISTS,
+  statusCodes
+}
